e33d1c51ba9e233753054359674ec7bcace24a40
Prashanthshah <shahprashanth4@gmai.com>
2022-01-06 22:47:13 +0530

    added route for frontend switch controll

--- routes/webhooks.route.js
+++ routes/webhooks.route.js
@@ -1,5 +1,5 @@
-const { default: axios } = require('axios');
-const express = require('express');
+const { default: axios } = require("axios");
+const express = require("express");
 
 const router = express.Router();
 
@@ -8,33 +8,34 @@ let switch2 = false;
 let switch3 = false;
 let switch4 = false;
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   console.log("webhook hit!!");
   try {
-   res.status(200);
-   res.json({message: 'success'})
+    res.status(200);
+    res.json({
+      message: "success",
+      switches: { switch1, switch2, switch3, switch4 },
+    });
   } catch (error) {
     res.status(400);
     res.json({ message: error.message });
   }
 });
 
-
-router.post('/update-switches', (req, res) => {
+router.post("/update-switches", (req, res) => {
   try {
-  if(req.body){
-    switch1 = req.body.switch1;
-    switch2 = req.body.switch2;
-    switch3 = req.body.switch3;
-    switch4 = req.body.switch4;
-  }
-   res.status(200);
-   res.json({message: 'success'})
+    if (req.body) {
+      switch1 = req.body.switch1;
+      switch2 = req.body.switch2;
+      switch3 = req.body.switch3;
+      switch4 = req.body.switch4;
+    }
+    res.status(200);
+    res.json({ message: "success" });
   } catch (error) {
     res.status(400);
     res.json({ message: error.message });
   }
 });
-
 
 module.exports = router;
