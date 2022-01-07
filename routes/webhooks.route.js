697995b104faea34bfa0035f048b33935580175e
Prashanth_Shah <55656506+Prashanthshah@users.noreply.github.com>
2022-01-07 13:45:36 +0530

    Update webhooks.route.js

--- routes/webhooks.route.js
+++ routes/webhooks.route.js
@@ -3,10 +3,10 @@ const express = require("express");
 
 const router = express.Router();
 
-let switch1 = false;
-let switch2 = false;
-let switch3 = false;
-let switch4 = false;
+let switch1 = 0;
+let switch2 = 0;
+let switch3 = 0;
+let switch4 = 0;
 
 router.get("/", (req, res) => {
   console.log("webhook hit!!");
@@ -25,10 +25,10 @@ router.get("/", (req, res) => {
 router.post("/update-switches", (req, res) => {
   try {
     if (req.body) {
-      switch1 = req.body.switch1;
-      switch2 = req.body.switch2;
-      switch3 = req.body.switch3;
-      switch4 = req.body.switch4;
+      switch1 = req.body.switch1 === true ? 1 : 0;
+      switch2 = req.body.switch2 === true ? 1 : 0;
+      switch3 = req.body.switch3 === true ? 1 : 0;
+      switch4 = req.body.switch4 === true ? 1 : 0;
     }
     res.status(200);
     res.json({ message: "success" });
